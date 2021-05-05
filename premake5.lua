6e1fa3525bd08e8865cce6d9d533d4fa1aa03d37
FFuuZZuu <47675688+FFuuZZuu@users.noreply.github.com>
2021-05-05 20:58:13 +0100

    Static Linking

--- premake5.lua
+++ premake5.lua
@@ -1,9 +1,11 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	targetdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
@@ -22,11 +24,14 @@ project "ImGui"
 
 	filter "system:windows"
 		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
 	
 	filter "system:linux"
 		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
-		pic "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
